801ea6bae805f0f730a40f98697d8406c2a7962e
Nazia Nafis <81024656+nazianafis@users.noreply.github.com>
2021-10-19 00:34:44 +0530

    Create 98-Validate-Binary-Search-Tree.py

---
+++ 98-Validate-Binary-Search-Tree.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        stack, inorder = [], float('-inf')
+        
+        while stack or root:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            if root.val <= inorder:
+                return False
+            inorder = root.val
+            root = root.right
+
+        return True
